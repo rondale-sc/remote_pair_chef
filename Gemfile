8c862f12c8169becf1e314218738a1710518ea55
Jonathan Jackson <jonj@promedicalinc.com>
2013-04-16 20:10:20 +0000

    Use librarian to store repos.

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
 source "https://rubygems.org"
 
 gem "chef", "~> 11.4.0"
-
+gem "librarian-chef", "~> 0.0.1"
--- cookbooks/remote_pair/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'remote_pair'
-maintainer       'Jonathan Jackson, Paul Welch'
-maintainer_email 'jonathan.jackson1@me.com'
-license          'All rights reserved'
-description      'Installs/Configures remote_pair'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
