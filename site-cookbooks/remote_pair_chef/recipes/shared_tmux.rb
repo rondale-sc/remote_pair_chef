79fef06911a422ba88a3ccd77be4114c8cf564bc
Robert Jackson <robert.w.jackson@me.com>
2013-04-18 15:47:02 -0400

    Run shared_tmux as 'pair' user.

--- site-cookbooks/remote_pair_chef/recipes/shared_tmux.rb
+++ site-cookbooks/remote_pair_chef/recipes/shared_tmux.rb
@@ -2,15 +2,17 @@
 ## Recipe   :: shared_tmux
 #
 
+include_recipe "remote_pair_chef::users"
 include_recipe "tmux"
 
 file '/tmp/pair' do
-  owner 'ubuntu'
+  owner 'pair'
   mode '0777'
   action :create
 end
 
 execute "Startup shared tmux session" do
+  user "pair"
   command "/usr/bin/tmux -S /tmp/pair new-session -d -s pairing"
   not_if { system('/usr/bin/tmux has -t pairing')  }
 end
