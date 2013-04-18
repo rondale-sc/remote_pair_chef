83ccbec08d6f98a5924b741f32357abccc7baac8
Robert Jackson <robert.w.jackson@me.com>
2013-04-18 10:28:56 -0400

    Try forcing tmux sessions.

--- site-cookbooks/remote_pair_chef/recipes/shared_tmux.rb
+++ site-cookbooks/remote_pair_chef/recipes/shared_tmux.rb
@@ -4,14 +4,14 @@
 
 include_recipe "tmux"
 
-execute "Startup shared tmux session" do
-  command "/usr/bin/tmux -S /tmp/pair new-session -d -s pairing"
-  not_if { system('/usr/bin/tmux has -t pairing')  }
-end
-
 file '/tmp/pair' do
   owner 'ubuntu'
   mode '0777'
   action :create
+end
+
+execute "Startup shared tmux session" do
+  command "/usr/bin/tmux -S /tmp/pair new-session -d -s pairing"
+  not_if { system('/usr/bin/tmux has -t pairing')  }
 end
 
