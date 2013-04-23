251f3a0da8ce6239ce8bd81b5c2e2e6c59422020
Robert Jackson <robert.w.jackson@me.com>
2013-04-23 18:18:58 -0400

    Refactor dotdotdot to use template for config.

--- site-cookbooks/remote_pair_chef/recipes/dotdotdot.rb
+++ site-cookbooks/remote_pair_chef/recipes/dotdotdot.rb
@@ -4,19 +4,20 @@ users.each do |user_id|
 
   home_dir = "/home/#{user['username']}"
 
-  git home_dir do
+  git "#{home_dir}/..." do
     repository 'git://github.com/ingydotnet/....git'
     reference 'master'
     action :sync
   end
 
-  remote_file "#{home_dir}/.../conf" do
-    source 'https://gist.github.com/rondale-sc/23fa89650bc89b61294d/raw/2b427d3346100b59cf8f9a5d2e9c8288d80f572c/conf'
+  template "#{home_dir}/.../conf" do
+    user user["username"]
+    source "dotdotdot.conf.erb"
   end
 
   execute 'sync dotfiles' do
     user user['username']
     cwd home_dir
-    command "#{home_dir}/.../bin/... supi"
+    command "#{home_dir}/.../bin/... super_update_install"
   end
 end
