a24e51c03deec852c9ba9d7ee3d0cc8d01f2ccda
Jonathan Jackson <jonj@promedicalinc.com>
2013-04-23 09:11:13 -0400

    Clean up a broken file!

--- site-cookbooks/remote_pair_chef/recipes/users.rb
+++ site-cookbooks/remote_pair_chef/recipes/users.rb
@@ -11,7 +11,7 @@ default_command = node["remote_pair_chef"]["default_ssh_command"]
 
 users.each do |user_id|
   user = data_bag_item("users", user_id)
-  home_dir = "/home/#{user['username']}/"
+  home_dir = "/home/#{user['username']}"
 
   user_account user["username"] do
     comment   user["comment"]
@@ -25,13 +25,13 @@ users.each do |user_id|
     action :sync
   end
 
-  remote_file "#{home_dir}.../conf" do
+  remote_file "#{home_dir}/.../conf" do
     source 'https://gist.github.com/rondale-sc/23fa89650bc89b61294d/raw/2b427d3346100b59cf8f9a5d2e9c8288d80f572c/conf'
   end
 
   execute 'sync dotfiles' do
     user user['username']
     cwd home_dir
-    command "#{home_dir}.../bin/... supi"
+    command "#{home_dir}/.../bin/... supi"
   end
 end
